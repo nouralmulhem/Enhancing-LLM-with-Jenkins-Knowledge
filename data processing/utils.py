60797963c1b4e9a96ead6cfbbc1667e4ee82c0c4
Nour Almulhem <nouralmulhem@gmail.com>
2024-06-12 00:24:17 +0300

    [FEAT] get community questions using discourse apis

---
+++ data processing/utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def remove_html_tags(text):
+    if pd.isna(text):  
+        return text
+
+    soup = BeautifulSoup(text, 'html.parser')
+
+    # Find all tags except <code>
+    for tag in soup.find_all(True):
+        if tag.name != 'code':
+            tag.unwrap()  # Remove the tag, keep its contents
+
+    return str(soup)
